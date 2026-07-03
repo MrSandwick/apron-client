48f979f086e99072270e6fe1f0e97c2daa1c07b6
Baatyrbek <batrudinovshapiro@gmail.com>
2026-07-03 03:28:35 -0500

    feat: build home promo section
    Add the Home promo card (app-home-promo) matching the Figma design:
    Limited Time badge, "Unlimited KBBQ Night" heading, price line, lorem
    copy, a placeholder "Claim This Deal" button, and a "See Full Menu" link
    to /menu. The HOT DEAL badge uses an inline SVG flame instead of the
    design's emoji.

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { HomeHero } from './hero/hero';
+import { HomePromo } from './promo/promo';
 
 @Component({
   selector: 'app-home',
-  imports: [HomeHero],
+  imports: [HomeHero, HomePromo],
   templateUrl: './home.html',
 })
 export class Home {}
---
+++ src/app/pages/home/promo/promo.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-home-promo',
+  imports: [RouterLink],
+  templateUrl: './promo.html',
+})
+export class HomePromo {}
